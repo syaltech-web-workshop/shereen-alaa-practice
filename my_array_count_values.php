77a0ac290a7cbf499d5a65195202de921edd5f43
sheriaa <41862932+sheriaa@users.noreply.github.com>
2018-08-12 19:56:09 +0200

    Add files via upload

--- my_array_count_values.php
+++ my_array_count_values.php
@@ -1,1 +1,40 @@
+<?php
 
+echo "Enter elements in array: ";
+for($i=0;$i<100;$i++) {
+    scanf($arr[i], "%x", $i);
+    $freq[$i] = -1;
+}
+
+
+for($i=0; $i<100; $i++)
+    {
+        $count = 1;
+        for($j=$i+1; $j<100; $j++)
+        {
+            
+            if($arr[$i]==$arr[$j])
+            {
+                $count++;
+
+            
+                $freq[$j] = 0;
+            }
+        }
+
+        if($freq[$i] != 0)
+        {
+            $freq[$i] = $count;
+        }
+    }
+
+echo "\nFrequency of all elements of array : \n";
+for($i=0; $i<100; $i++)
+    {
+        if($freq[$i] != 0)
+        {
+            echo "%x occurs %x times\n", $arr[$i], $freq[$i];
+        }
+    }
+
+return 0 ;
